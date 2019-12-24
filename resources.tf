612f1dafb251b5bbce1e1809819623d3cf3d2878
Raj <rajasekhar.reddy@nike.com>
2019-12-24 13:00:01 -0800

    build1

--- resources.tf
+++ resources.tf
@@ -6,3 +6,9 @@ tags = {
   Name = "terraform-aws-vpc-ex-one"
   }
 }
+
+resource "aws_subnet" "subnet1" {
+cidr_block = "${cidrsubnet(aws_vpc.environment-ex-one.cidr_block, 3, 1)}"
+vpc_id = "${aws_vpc.environment-ex-one.id}"
+availability_zone = "us-east-1a"
+}
